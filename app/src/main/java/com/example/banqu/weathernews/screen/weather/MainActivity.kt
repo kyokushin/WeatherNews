e85cb3c9de8b3c4a6ba641ff2d202b280699fdc2
yuta suzuki <banquet.of.merry.widow.y.s@gmail.com>
2018-03-19 20:52:05 +0900

    ファイルのパスを変更した

--- app/src/main/java/com/example/banqu/weathernews/screen/weather/MainActivity.kt
+++ app/src/main/java/com/example/banqu/weathernews/screen/weather/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.banqu.weathernews
+package com.example.banqu.weathernews.screen.weather
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.banqu.weathernews.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
--- app/src/main/java/com/example/banqu/weathernews/screen/weather/MainActivityFragment.kt
+++ app/src/main/java/com/example/banqu/weathernews/screen/weather/MainActivityFragment.kt
@@ -1,10 +1,11 @@
-package com.example.banqu.weathernews
+package com.example.banqu.weathernews.screen.weather
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.banqu.weathernews.R
 
 /**
  * A placeholder fragment containing a simple view.
