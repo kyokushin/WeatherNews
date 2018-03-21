82b275401901f9f5ab49226e32b9da73a5add592
yuta suzuki <banquet.of.merry.widow.y.s@gmail.com>
2018-03-21 22:02:53 +0900

    天気アイコンを追加

---
+++ app/src/main/java/com/example/banqu/weathernews/screen/common/WeatherIcon.kt
@@ -0,0 +1,50 @@
+package com.example.banqu.weathernews.screen.common
+
+import com.example.banqu.weathernews.R
+
+/**
+ * Created by banqu on 2018/03/21.
+ */
+object WeatherIcon {
+
+    // livedoor は1-30までのアイコンがある
+    val livedoor = arrayOf(
+            emptyArray(),//使用しない
+            // 1
+            arrayOf(R.drawable.ic_weather_sunny), //晴れ
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy), //晴れ時々曇り
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy), //晴れのち曇り
+            //6
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            //11
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            //16
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            //21
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            //26
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy),
+            arrayOf(R.drawable.ic_weather_sunny, R.drawable.ic_weather_cloudy)
+    )
+}
